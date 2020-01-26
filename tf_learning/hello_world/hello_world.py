e0ed5abd8793c0536983fd977e0a4fb204eedc11
张涛 <zhixu.zt@taobao.com>
2020-01-26 18:17:13 +0800

    #1 add hello world

---
+++ tf_learning/hello_world/hello_world.py
@@ -0,0 +1,18 @@
+# -*- coding:utf-8 -*-
+
+import tensorflow as tf
+
+a = tf.constant([2])
+b = tf.constant([3])
+
+c = tf.add(a, b)
+
+
+# session = tf.Session()
+# result = session.run(c)
+# print(result)
+# session.close()
+
+with tf.Session() as session:
+    result = session.run(c)
+    print result
